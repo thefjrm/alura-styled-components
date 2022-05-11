5f6a1994606ba2064dc8ff6b12b621a4ac3e9b3c
thefjrm <felipemartinsplayernet@gmail.com>
2022-05-11 19:14:53 -0300

    Feature de Extratos

--- src/Components/Extrato/index.jsx
+++ src/Components/Extrato/index.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 import { Box, Botao } from "../UI";
 import { extratoLista } from "../../info";
+import Items from "../Items";
 
 export const Extrato = () => {
   return (
     <Box>
       {extratoLista.updates.map(({ id, type, from, value, date }) => {
         return (
-          <div key={id}>
-            <div>{type}</div>
-            <div>{from}</div>
-          </div>
+          <Items key={id} type={type} from={from} value={value} date={date} />
         );
       })}
       <Botao>Ver Mais</Botao>
